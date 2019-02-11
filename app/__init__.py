267e72a685e6e2e4c3c17f3c606df672dac1d382
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2019-02-11 10:20:46 +0100

    - fixed root route handling - moved swagger to /doc/

--- app/__init__.py
+++ app/__init__.py
@@ -10,9 +10,16 @@ app = Flask(__name__)
 app.config.from_object(BaseConfig)
 app.config.from_envvar('APP_CONFIG')
 
+
+# register before creating API to avoid overwriting
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
-api = Api(app)
+api = Api(app, doc='/doc/')
 
 # import here to have db and api defined already
 from app.models import *  # noqa
@@ -30,8 +37,3 @@ def load_user(id):
 @app.template_filter('date')
 def date_filter(s):
     return s.split('T')[0] if s is not None else s
-
-
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
